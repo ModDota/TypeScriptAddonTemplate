735e676ba02bd0b679b39f82cc603a16ff1f18a7
ark120202 <ark120202@gmail.com>
2020-03-14 22:53:13 +0500

    Add modifier motion classes to ts adapter

--- game/scripts/vscripts/lib/dota_ts_adapter.ts
+++ game/scripts/vscripts/lib/dota_ts_adapter.ts
@@ -17,6 +17,15 @@ export class BaseModifier {
     }
 }
 
+export interface BaseModifierMotionHorizontal extends CDOTA_Modifier_Lua_Horizontal_Motion {}
+export class BaseModifierMotionHorizontal extends BaseModifier {}
+
+export interface BaseModifierMotionVertical extends CDOTA_Modifier_Lua_Vertical_Motion {}
+export class BaseModifierMotionVertical extends BaseModifier {}
+
+export interface BaseModifierMotionBoth extends CDOTA_Modifier_Lua_Motion_Both {}
+export class BaseModifierMotionBoth extends BaseModifier {}
+
 export const registerAbility = (name: string) => (ability: new () => CDOTA_Ability_Lua | CDOTA_Item_Lua) => {
     const [env] = getFileScope();
 
@@ -37,9 +46,7 @@ export const registerAbility = (name: string) => (ability: new () => CDOTA_Abili
     };
 };
 
-export const registerModifier = (name: string, type = LuaModifierType.LUA_MODIFIER_MOTION_NONE) => (
-    modifier: new () => CDOTA_Modifier_Lua,
-) => {
+export const registerModifier = (name: string) => (modifier: new () => CDOTA_Modifier_Lua) => {
     (modifier as any).name = name;
 
     const [env, source] = getFileScope();
@@ -60,6 +67,20 @@ export const registerModifier = (name: string, type = LuaModifierType.LUA_MODIFI
             originalOnCreated.call(this, parameters);
         }
     };
+
+    let type = LuaModifierType.LUA_MODIFIER_MOTION_NONE;
+    let base = (modifier as any).____super;
+    while (base && type === undefined) {
+        if (base === BaseModifierMotionBoth) {
+            type = LuaModifierType.LUA_MODIFIER_MOTION_BOTH;
+        } else if (base === BaseModifierMotionHorizontal) {
+            type = LuaModifierType.LUA_MODIFIER_MOTION_HORIZONTAL;
+        } else if (base === BaseModifierMotionVertical) {
+            type = LuaModifierType.LUA_MODIFIER_MOTION_VERTICAL;
+        }
+
+        base = base.____super;
+    }
 
     LinkLuaModifier(name, fileName, type);
 };
